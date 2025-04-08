80aeccd2972d6d3370b68dece9477e0cb5b722cf
yniverz <88168412+yniverz@users.noreply.github.com>
2025-04-09 01:18:45 +0200

    Initial commit

---
+++ WayPointDB-iOS/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  WayPointDB-iOS
+//
+//  Created by yniverz on 09.04.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+            
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WayPointDB-iOS/WayPointDB_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  WayPointDB_iOSApp.swift
+//  WayPointDB-iOS
+//
+//  Created by yniverz on 09.04.25.
+//
+
+import SwiftUI
+
+@main
+struct WayPointDB_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
